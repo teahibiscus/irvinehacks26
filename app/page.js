486115d55475d25375319686c0b0bbedd0650ac3
teahibiscus <109654372+teahibiscus@users.noreply.github.com>
2026-03-01 01:16:54 -0800

    fixed both postcards!

--- app/page.js
+++ app/page.js
@@ -71,34 +71,19 @@ export default function Home() {
       >
         <div className="p-8">
           <h1>Create a Postcard</h1>
-          <div className="bg-[url('/card_side1.jpg')] w-[800px] h-[530px] mb-4 text-gray-600 flex justify-end items-center">
-            <div className="w-1/2">
+          <div className="bg-[url('/card_front.jpg')] w-[800px] h-[530px] mb-4 text-gray-600 flex justify-end items-center">
+            <div className="w-1/2 h-full flex items-center justify-center p-4">
               <img
                 src={preview}
                 alt="Preview"
-                className="max-h-full object-contain"
+                className="max-w-full max-h-full object-contain"
               />
             </div>
             <div className="w-1/3 ml-auto">
               <SmallEditor ref={receiverRef} placeholder="Receiver's Name" />
             </div>
           </div>
-
-          <SmallEditor ref={titleRef} placeholder="Title your postcard!" />
           <TextEditor ref={bodyRef} placeholder="Enter your message here." />
-
-          <div className="mt-8">
-            <button
-              onClick={onSaveButtonClick}
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-            >
-              <img
-                src="/window.svg"
-                alt="Save Icon"
-                className="inline-block w-5 h-5 mr-2"
-              />
-            </button>
-          </div>
         </div>
         <Footer
           onImageSelect={handleImageSelect}
